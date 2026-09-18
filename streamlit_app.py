2bd4dfc25dd959ed736272e818b7ad6591f74e85
Oskar <71490898+OskarJot@users.noreply.github.com>
2026-09-18 09:34:54 +0200

    Connect to Snowflake and clean up code
    Establish connection to Snowflake and remove duplicate connection code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+conn = st.connection("snowflake")
+session = conn.session()
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -46,6 +48,3 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-
-cnx = st.connection("snowflake")
-session = cnx.session()
