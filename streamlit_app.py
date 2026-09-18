a38bed9715b111a87360c1d8854dd657dc1cc2c3
Oskar <71490898+OskarJot@users.noreply.github.com>
2026-09-18 15:13:01 +0200

    Comment out Pandas conversion and update API request
    Comment out the conversion of Snowpark DataFrame to Pandas and related Streamlit calls. Update API request to use a dynamic fruit name based on user selection.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 # Convert the Snowpark to Pandas
-pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#pd_df = my_dataframe.to_pandas()
+#st.dataframe(pd_df)
+#st.stop()
 
 # Convert Snowpark DataFrame column to a standard Python list
 fruit_options_list = [row['FRUIT_NAME'] for row in my_dataframe.collect()]
@@ -49,7 +49,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
