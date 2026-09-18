1b10ceb426941dd7ac902a3ed6d7b32af5fde99d
Oskar <71490898+OskarJot@users.noreply.github.com>
2026-09-18 14:35:32 +0200

    Eliminate redundant API call in smoothie order
    Removed duplicate API call for smoothie nutrition information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
@@ -45,7 +48,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
