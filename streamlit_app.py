1fdd4b987c38dd42fb36847574b40edd64c2ba3e
Oskar <71490898+OskarJot@users.noreply.github.com>
2026-09-18 15:08:35 +0200

    Fix dataframe display in Streamlit app

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert the Snowpark to Pandas
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_pf)
+st.dataframe(pd_df)
 st.stop()
 
 # Convert Snowpark DataFrame column to a standard Python list
