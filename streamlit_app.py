368b3d5abff964e48b9bea9df4f7e242444e42c4
Oskar <71490898+OskarJot@users.noreply.github.com>
2026-09-18 15:14:34 +0200

    Uncomment code to display Pandas DataFrame

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 # st.stop()
 
 # Convert the Snowpark to Pandas
-#pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 #st.stop()
 
 # Convert Snowpark DataFrame column to a standard Python list
